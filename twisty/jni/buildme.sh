cc9e1431fec756dfb84e08c52dcabdda3c458903
Stephen Gutknecht <stephen.gutknecht@gmail.com>
2016-10-22 17:24:10 -0400

    rebuild NDK binaries with NDK 12b, Git Interpreter 1.3.4

---
+++ twisty/jni/buildme.sh
@@ -0,0 +1,5 @@
+echo "Twisty NDK 12b build and deply"
+/spot/WorkDev0/devPlace0/android_dev0/android-ndk-r12b/ndk-build
+rsync -aAXv --progress ../libs/ ../../app/src/main/jniLibs/
+rm -r ../../build/
+rm -r ../../app/build/
